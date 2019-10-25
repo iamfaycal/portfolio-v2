e04418c31f686253ac41e3d622196fc222836eab
Fayçal Hammoudi <fay68390@gmail.com>
2019-10-25 14:30:00 +0200

    edit about text

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -8,13 +8,10 @@ const About = () => {
         <section id="about">
             <h2 className="sectionTitle">à propos</h2>
             <p className="about__text">
-                Hello, je suis Fayçal, développeur web créatif
-                <br />
-                dans le secteur de Mulhouse. J'aide les petites entreprises
-                <br />
-                qui souhaitent se démarquer en ligne à créer
-                <br />
-                des expériences uniques pour leurs visiteurs.
+                Hello, je suis Fayçal, développeur web créatif dans le secteur
+                de Mulhouse. J'aide les petites entreprises qui souhaitent se
+                démarquer en ligne à créer des expériences uniques pour leurs
+                visiteurs.
             </p>
             <a className="underlineLink" href="#work">
                 voir mes dernières réalisations
